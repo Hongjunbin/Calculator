5478a1cddcf5249f7af778b6816a205f9d05b283
junbin <ghdwnsqls0@naver.com>
2024-04-25 22:18:48 +0900

    1 Requirements

--- src/App.java
+++ src/App.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        int number1 = sc.nextInt();
+        System.out.println(number1);
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        int number2 = sc.nextInt();
+        sc.nextLine();
+        System.out.println(number2);
+
+    }
 }
