e1e6f985fd76f7f19d09b2e61a70c906f91b56ee
junbin <ghdwnsqls0@naver.com>
2024-04-29 18:42:40 +0900

    lv2-5,6,7

--- src/App.java
+++ src/App.java
@@ -3,38 +3,52 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Calculator c = new Calculator(new ArrayList<Integer>());
+        Calculator c = new Calculator(new ArrayList<Double>(), new ArrayList<Integer>());
 
         Scanner sc = new Scanner(System.in);
 
         boolean turnOnOff = true;
 
         while(turnOnOff) {
-            System.out.print("첫 번째 숫자를 입력하세요:");
-            int num1 = sc.nextInt();
-            System.out.print("두 번째 숫자를 입력하세요:");
-            int num2 = sc.nextInt();
+            System.out.println("1. 사칙연산 하기");
+            System.out.println("2. 원의넓이 구하기");
+            String check = sc.nextLine();
 
-            System.out.print("사칙연산 기호를 입력하세요: ");
-            char operator = sc.next().charAt(0);
-            sc.nextLine();
-            try {
-                // 계산된 값 setter주입 메서드 안에서 직접 add메서드 호출 후 주입.
-                c.setList(c.calculate(operator, num1, num2));
-            } catch(CalculaotrException e) {
-                System.out.println(e.getMessage());
+            if(check.equals("1")) {
+                System.out.print("첫 번째 숫자를 입력하세요:");
+                int num1 = sc.nextInt();
+                System.out.print("두 번째 숫자를 입력하세요:");
+                int num2 = sc.nextInt();
+
+                System.out.print("사칙연산 기호를 입력하세요: ");
+                char operator = sc.next().charAt(0);
+                sc.nextLine();
+                try {
+                    // 계산된 값 setter주입 메서드 안에서 직접 add메서드 호출 후 주입.
+                    c.setList(c.calculate(operator, num1, num2));
+                } catch(CalculaotrException e) {
+                    System.out.println(e.getMessage());
+                }
+
+                System.out.println(c.getList());
+                System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
+                if(sc.nextLine().equals("remove")){
+                    c.removeResult();
+                }
+
+                System.out.println(c.getList());
+                System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+
+                turnOnOff = !sc.nextLine().equals("exit");
+            } else if(check.equals("2")) {
+                System.out.println("반지름을 입력해주세요.");
+                int radius = Integer.parseInt(sc.nextLine());
+                c.setCircle(c.calculateCircleArea(radius));
+                System.out.println("원의 넓이 결과 : " + c.getCircle());
+            } else {
+                System.out.println("잘못 입력했습니다. 다시 선택해주세요.");
             }
 
-            System.out.println(c.getList());
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
-            if(sc.nextLine().equals("remove")){
-                c.removeResult();
-            }
-
-            System.out.println(c.getList());
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-
-            turnOnOff = !sc.nextLine().equals("exit");
         }
     }
 }
--- src/Calculator.java
+++ src/Calculator.java
@@ -2,9 +2,14 @@ import java.util.ArrayList;
 
 public class Calculator {
 
+    public static final double PI = 3.14;
+    // 절대 변하지 않는 수 => 상수 즉, PI(파이)는 3.141592...으로 변하지 않는 수 이다.
+    private final ArrayList<Double> circle;
+
     private final ArrayList<Integer> list;
 
-    public Calculator(ArrayList<Integer> list){
+    public Calculator(ArrayList<Double> circle, ArrayList<Integer> list){
+        this.circle = circle;
         this.list = list;
     }
 
@@ -18,6 +23,20 @@ public class Calculator {
 
     public void removeResult() {
         this.list.remove(0);
+    }
+
+    /* 원의 넓이를 구하는 메서드 선언*/
+    public double calculateCircleArea(int radius) {
+        /* 원의 넓이 계산 구현 */
+        return PI * (radius * radius);
+    }
+    /* 원의 넓이 저장 필드 Getter, Setter, 조회 메서드 구현 */
+    public void setCircle(double circle) {
+        this.circle.add(circle);
+    }
+
+    public ArrayList<Double> getCircle() {
+        return circle;
     }
 
     public int calculate(char sign, int num1, int num2) throws CalculaotrException {
