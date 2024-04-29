fb02e31dcd65c5da40c85730fae5fccfa7f089bc
junbin <ghdwnsqls0@naver.com>
2024-04-29 10:33:19 +0900

    lv1-4

--- src/App.java
+++ src/App.java
@@ -2,33 +2,40 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        System.out.print("첫 번째 숫자를 입력하세요: ");
-        int number1 = sc.nextInt();
+        boolean turnOnOff = true;
+        while(turnOnOff){
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int number1 = sc.nextInt();
 
-        System.out.print("두 번째 숫자를 입력하세요: ");
-        int number2 = sc.nextInt();
-        sc.nextLine();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int number2 = sc.nextInt();
+            sc.nextLine();
 
-        System.out.print("사칙연산 기호를 입력하세요: ");
-        String sign = sc.nextLine();
-        System.out.println(sign.charAt(0));
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            String sign = sc.nextLine();
 
-        int result = 0;
-        if(sign.equals("+")){
-            result = number1 + number2;
-        } else if(sign.equals("-")) {
-            result = number1 - number2;
-        } else if(sign.equals("*")) {
-            result = number1 * number2;
-        } else if(sign.equals("/")) {
-            if(number2 != 0) {
-                result = number1 / number2;
-            } else {
-                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            int result = 0;
+            if(sign.charAt(0) == '+'){
+                result = number1 + number2;
+            } else if(sign.charAt(0) == '-') {
+                result = number1 - number2;
+            } else if(sign.charAt(0) == '*') {
+                result = number1 * number2;
+            } else if(sign.charAt(0) == '/') {
+                if(number2 != 0) {
+                    result = number1 / number2;
+                } else {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                }
             }
+            System.out.println("결과 : " + result);
+
+            System.out.println("더 계산하시겠습니까? (exit입력 시 종료)");
+            turnOnOff = !sc.nextLine().equals("exit");
         }
-        System.out.println("결과 : " + result);
+
     }
 }
